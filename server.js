922a6aa5719df3f8f0c979dd4a40c1a64ea89ad5
Brianna Gaines <briannagaines31@gmail.com>
2022-06-07 20:09:08 -0600

    adds routes

--- server.js
+++ server.js
@@ -31,3 +31,44 @@ const TherapistSchema = new mongoose.Schema({
 })
 
 const Therapist = mongoose.model("Therapist", TherapistSchema)
+
+app.get("/", (req,res) =>{
+    res.send ("you are home")
+})
+
+app.get("/therapists", async (req,res)=>{
+    try{
+        res.json(await Therapist.find({}))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+app.post("/therapists", async (req, res) => {
+    try {
+      res.json(await Therapist.create(req.body))
+    } catch (error) {
+      res.status(400).json(error)
+    }
+  })
+
+app.delete("/therapists/:id", async (req, res) => {
+    try {
+      res.json(await Therapist.findByIdAndDelete(req.params.id))
+    } catch (error) {
+      res.status(400).json(error)
+    }
+  })
+  
+  app.put("/therapists/:id", async (req, res) => {
+    try {
+      res.json(
+        await Therapist.findByIdAndUpdate(req.params.id, req.body, { new: true })
+      )
+    } catch (error) {
+      res.status(400).json(error)
+    }
+  })
+
+
+app.listen(PORT, ()=> console.log(`listening on ${PORT}`))
